17331774ead7901330309e269f22e00ec677773e
m010-cpu <81363508+m010-cpu@users.noreply.github.com>
2024-05-20 01:57:08 +0200

    update wezterm opacity

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -31,7 +31,7 @@ config.colors = {
 }
 
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.8
 config.macos_window_background_blur = 20
 
 config.window_close_confirmation = "NeverPrompt"
